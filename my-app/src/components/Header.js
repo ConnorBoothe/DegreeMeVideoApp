544cfccc4873cf2e2c4a84bf7115fceda8eae4a2
Connor Boothe <connorboothe@gmail.com>
2021-04-27 19:57:09 -0400

    Created directory to store components

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import logo from "../images/degreeMeLogo.svg";
+import "../css/Header.css";
+// import bootstrap from "bootstrap";
+class Header extends Component {
+  render(){
+    return (
+      
+        <div className="header">
+          <ul>
+            <li className="logo-link"><img className="site-logo" src={logo}/></li>
+            <li className="coming-soon-text">DegreeMe Video Collection Tool</li>
+          </ul>
+        </div>
+        // <p>Coming soon to iOs</p>
+    );
+
+
+  }
+}
+
+export default Header;
