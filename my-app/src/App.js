323030155ce2e705153398f744361a69d7283b76
Connor Boothe <connorboothe@gmail.com>
2021-04-29 20:09:00 -0400

    Added Router to App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Header from './components/Header';
 import VideoUploader from './components/VideoUploader';
+import Home from './components/Home';
+import Login from './components/Login';
+import CreateAccount from './components/CreateAccount';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
 
     <div className="App">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <Header />
-      <VideoUploader />
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+              <Redirect to="/Home" />
+            </Route>
+            <Route exact path="/Home" component={Home} />
+            <Route exact path="/Upload" component={VideoUploader} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/CreateAccount" component={CreateAccount} />
+
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
