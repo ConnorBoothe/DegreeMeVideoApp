0ace867f01b5f1fb7406c68888dfb0b8a4e3e4a5
Connor Boothe <connorboothe@gmail.com>
2021-04-29 20:06:33 -0400

    Created default properties for verification code, subscription_level, and image

--- Models/User.js
+++ Models/User.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     First_Name: {type:String, required: true},
-    Creator_Email:{type:String, required: true},
     Last_Name:{type:String, required: true},
     Email:{type:String, required:true},
     Password:{type:String, required:true},
@@ -22,21 +21,31 @@ var UserDB = mongoose.model('User',UserSchema);
 module.exports = class User {
      //add user to db
      addUser(First_Name, Last_Name, Email, Password,
-      Image, Subscription_Level, Verification_Code){
+        Subscription_Level){
         return new Promise((resolve, reject)=>{
+        const defaultImages = [
+            "https://firebasestorage.googleapis.com/v0/b/degreeme-bd5c7.appspot.com/o/default-profile-images%2Fcircle_degreeMe_logo_4.png?alt=media&token=94de9ef2-573a-4b1f-a5c3-edf56c1855cc",
+            "https://firebasestorage.googleapis.com/v0/b/degreeme-bd5c7.appspot.com/o/default-profile-images%2Fcircle_degreeMe_logo_2.png?alt=media&token=dc792ec6-9909-4ef8-ba6f-56b87818cdee",
+            "https://firebasestorage.googleapis.com/v0/b/degreeme-bd5c7.appspot.com/o/default-profile-images%2Fcircle_degreeMe_logo_1.png?alt=media&token=2fa89c02-1341-414a-84a1-943ee00fde6b",
+            "https://firebasestorage.googleapis.com/v0/b/degreeme-bd5c7.appspot.com/o/default-profile-images%2Fcircle_degreeMe_logo.png?alt=media&token=c56774ca-891b-47b9-9616-a3011347e78b",
+        ];
+        var verificationCode = Math.floor(Math.random() * 10000);
+        //get random number between 0-3 and select that index of defaultImages array
+        var randomNum = Math.floor(Math.random() * Math.floor(4));
+        var randomImg = defaultImages[randomNum];
           var user = new UserDB({
             First_Name: First_Name,
             Last_Name: Last_Name,
             Email: Email,
             Password: Password,
-            Image: Image,
-            Subscription_Level: Subscription_Level,
-            Verification_Code: Verification_Code,
+            Image: randomImg,
+            Subscription_Level: "Free Tier",
+            Verification_Code: verificationCode,
             Account_Verified: false
           })
           user.save()
-          .then(()=>{
-            console.log(video)
+          .then((user)=>{
+            console.log(user)
             resolve(user)
           })
           .catch((err)=>{
@@ -59,7 +68,8 @@ module.exports = class User {
     }
     //attempt to login user
     login(Email){
-        return UserDB.find(
+        console.log("Logging you in...")
+        return UserDB.findOne(
           {
             Email: Email,
           }
