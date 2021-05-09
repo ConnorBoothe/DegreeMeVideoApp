b97d9589e8b7626b59a4a54ca1370620325877cf
Connor Boothe <connorboothe@gmail.com>
2021-05-09 14:03:08 -0400

    Added temp route to get all videos

---
+++ Controllers/GetAllVideos.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true,
+    resave: false,
+    saveUninitialized: true
+}));
+var VideosDB = require('../Models/Videos');
+var videos = new VideosDB();
+//endpoint to add user to database
+
+router.get('/API/GetAllVideos', 
+    function(req, res){
+        videos.getAllVideos()
+        .then((videos)=>{
+            console.log("Retrieving vids")
+            console.log(videos)
+            res.json(videos);
+        })
+        .catch((err)=>{
+            console.log(err)
+            res.json(err)
+        })
+});
+module.exports = router;
