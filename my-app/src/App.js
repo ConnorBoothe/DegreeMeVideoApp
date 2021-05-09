f52e461c89792aa22801614ce683b015a43854ec
Connor Boothe <connorboothe@gmail.com>
2021-05-09 14:00:27 -0400

    Added new routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,10 @@ import Header from './components/Header';
 import VideoUploader from './components/VideoUploader';
 import Home from './components/Home';
 import Login from './components/Login';
+import SingleVideo from './components/SingleVideo';
 import CreateAccount from './components/CreateAccount';
+import User from './components/User';
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -23,6 +26,8 @@ function App() {
             <Route exact path="/Upload" component={VideoUploader} />
             <Route exact path="/Login" component={Login} />
             <Route exact path="/CreateAccount" component={CreateAccount} />
+            <Route exact path="/Video/:id" component={SingleVideo} />
+            <Route exact path="/User/:id" component={User} />
 
         </Switch>
       </Router>
