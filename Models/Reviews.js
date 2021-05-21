869f75730c4f997be9f1869734200ee23b1a207e
Connor Boothe <connorboothe@gmail.com>
2021-05-21 19:11:46 -0400

    Restricted user to one review per creator per month

--- Models/Reviews.js
+++ Models/Reviews.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true },function(err){
-    console.log("Connect")
-});
+// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true },function(err){
+// });
 var Schema = mongoose.Schema;
 var ReviewSchema = new Schema({
     Creator_Id:{type:String, required: true},
@@ -9,7 +8,8 @@ var ReviewSchema = new Schema({
     Author_First_Name:{type:String, required: true},
     Author_Last_Name:{type:String, required: true},
     Author_Img:{type:String, required: true},
-    Message:{type:Number, required:true},
+    Message:{type:String, required:true},
+    Rating:{type:Number, required: true},
     Date:{type:Date, required:true},
 }, {collection: 'Reviews'});
 var ReviewsDB= mongoose.model('Reviews', ReviewSchema);
@@ -17,7 +17,7 @@ var ReviewsDB= mongoose.model('Reviews', ReviewSchema);
 module.exports = class Reviews {
      //add review to db
      addReview(Creator_Id, Author_Id, Author_First_Name,
-         Author_Last_Name, Author_Img, Message, Date){
+         Author_Last_Name, Author_Img, Message, Rating, Date){
         return new Promise((resolve, reject)=>{
           var review = new ReviewsDB({
             Creator_Id: Creator_Id,
@@ -26,16 +26,32 @@ module.exports = class Reviews {
             Author_Last_Name: Author_Last_Name,
             Author_Img: Author_Img,
             Message: Message,
+            Rating: Rating,
             Date: Date
           })
-          review.save()
-          .then(()=>{
-            resolve(review)
+          ReviewsDB.find({
+            $and: [{
+              Creator_Id: Creator_Id,
+              Author_Id: Author_Id,
+            }]
+          })
+          .then((reviews)=>{
+            if(reviews.length < 1) {
+              review.save()
+              .then(()=>{
+                resolve(review)
+              })
+            }
+            else{
+              console.log("exists")
+              resolve(false)
+            }
           })
           .catch((err)=>{
             console.log(err)
             reject(err)
           })
+        
       })
      }
      //get creator reviews
