54aff03a9a7ef47e8c8ce8d42187868e71dc0571
Connor Boothe <connorboothe@gmail.com>
2021-05-21 19:11:18 -0400

    Added Likes and Notifications Models

---
+++ Models/Likes.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true },function(err){
+    
+});
+var Schema = mongoose.Schema;
+
+var LikesSchema = new Schema({
+    UserId: {type:String, required:true},
+    VideoId: {type:String, required:true},
+    First_Name: {type:String, required:true},
+    Last_Name: {type:String, required:true},
+    Image: {type:String, required:true},
+    Date: {type:Date, required:true}
+  }, {collection: 'Likes'});
+var LikesDB = mongoose.model('Likes', LikesSchema);
+
+module.exports = class Comment {
+     //add comment to db
+     addLike(VideoId, UserId, First_Name, Last_Name,
+         Image){
+        return new Promise((resolve, reject)=>{
+          //check if user has liked the video
+          LikesDB.find({
+              $and: [{
+                UserId: UserId,
+                VideoId, VideoId
+              }]
+            })
+          .then((likes)=>{
+            if(likes.length < 1){
+              var like = new LikesDB({
+                VideoId: VideoId,
+                UserId: UserId,
+                First_Name: First_Name,
+                Last_Name: Last_Name,
+                Image: Image,
+                Date: new Date()
+              })
+              like.save()
+              .then((like)=>{
+                resolve(like)
+              })
+              .catch((err)=>{
+                console.log(err)
+                resolve(false)
+              })
+            }
+            else{
+              resolve(false);
+            }
+          })
+          
+      })
+     }
+     getLikesByVideo(VideoId){
+        return LikesDB.find({VideoId: VideoId})
+     }
+     getLikesByUserId(UserId){
+       console.log(UserId)
+        return LikesDB.find({UserId: UserId}, "VideoId")
+     }
+     removeLike(UserId, VideoId){
+       return LikesDB.deleteOne({
+         $and: [
+           {UserId: UserId},
+           {VideoId: VideoId}
+          ]
+        })
+    }
+}
---
+++ Models/Notifications.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true },function(err){
+    
+});
+var Schema = mongoose.Schema;
+var NotificationSchema = new Schema({
+    UserId: {type:String, required:true},
+    Author_UserId: {type:String, required:true},
+    Author_First_Name: {type:String, required:true},
+    Author_Last_Name: {type:String, required:true},
+    Author_Image: {type:String, required:true},
+    Message: {type:String, required:true},
+    Date: {type:Date, required:true},
+    Viewed: {type: Boolean, required: true},
+    VideoId: {type:String}
+  }, {collection: 'Notifications'});
+var NotificationsDB = mongoose.model('Notifications', NotificationSchema);
+
+module.exports = class Notifications {
+     //add comment to db
+     addNotification(UserId, Author_UserId, Author_First_Name, Author_Last_Name,
+         Author_Image, Message, VideoId){
+           console.log("adding notif")
+        return new Promise((resolve, reject)=>{
+              var notification = new NotificationsDB({
+                UserId: UserId,
+                Author_UserId: Author_UserId,
+                Author_First_Name: Author_First_Name,
+                Author_Last_Name: Author_Last_Name,
+                Author_Image: Author_Image,
+                Message: Message,
+                Date: new Date(),
+                VideoId: VideoId, 
+                Viewed: false
+              })
+              notification.save()
+              .then((notif)=>{
+                console.log("resolve notif")
+                console.log(notif)
+                resolve(notif)
+              })
+              .catch((err)=>{
+                console.log(err)
+                reject(err)
+              })
+      })
+     }
+     getNotificationsByUserId(UserId){
+        return NotificationsDB.find({UserId: UserId})
+        .sort({"Date": -1})
+        
+     }
+}
