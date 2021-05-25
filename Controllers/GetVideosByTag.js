b22d531b6854ee16a7c5e1c1b11dd97bd92438cc
Connor Boothe <connorboothe@gmail.com>
2021-05-25 18:52:12 -0400

    Route to map video tags to videos

---
+++ Controllers/GetVideosByTag.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true,
+    resave: false,
+    saveUninitialized: true
+}));
+var TagsDB = require('../Models/Tags');
+var tags = new TagsDB();
+
+var VideosDB = require('../Models/Videos');
+var videos = new VideosDB();
+//get videos by tag
+router.get('/API/GetVideoByTag/:tag',
+    function (req, res) {
+        tags.getVideoIdsByTag(req.params.tag)
+        .then((videoIds)=>{
+            var videoIdArray = [];
+            for(var x = 0; x < videoIds.length; x++) {
+                videoIdArray.push(videoIds[x].VideoId)
+            }
+            console.log("Id array", videoIdArray)
+            videos.getVideosByIdArray(videoIdArray)
+            .then((videos)=>{
+                res.json(videos)
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json(err)
+        })
+    });
+module.exports = router;
