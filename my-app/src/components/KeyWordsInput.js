7f6bca6cfdf50fd56035f75c179d8e8ea2ebe571
Connor Boothe <connorboothe@gmail.com>
2021-06-11 14:54:19 -0400

    Added ul, li tags with index per React requirement

--- my-app/src/components/KeyWordsInput.js
+++ my-app/src/components/KeyWordsInput.js
@@ -12,17 +12,19 @@ class KeyWordsInput extends Component {
                 </div>
                 <p className="text-light your-keywords">Your keywords</p>
                 <div className="append-tags">
-                    {this.props.keywords.map((keyword, index) => (
-                        <li key={index}>
-                            <div className="tag-container" >
-                                <p key={index} className="tag text-light">
-                                <span className="text-light remove-tag" onClick={() => this.props.removeKeyword(index)}>x</span>
-                                    {keyword}
-                                </p>
-                            </div>
-                        </li>
-                        
-                    ))}
+                    <ul className="keywords-list">
+                        {this.props.keywords.map((keyword, index) => (
+                            <li key={index}>
+                                <div className="tag-container" >
+                                    <p key={index} className="tag text-light">
+                                    <span className="text-light remove-tag" onClick={() => this.props.removeKeyword(index)}>x</span>
+                                        {keyword}
+                                    </p>
+                                </div>
+                            </li>
+                            
+                        ))}
+                    </ul>
                      <p className="keywords-helper-text">Your keywords control the videos that populate your dashboard</p>
                 </div>
             </div>
